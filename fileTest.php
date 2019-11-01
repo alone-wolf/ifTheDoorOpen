9ba5072e2100f13288dcc8dec97cf622a9676382
WolfHugo <1031629044@qq.com>
2019-11-01 14:54:37 +0800

    final

--- fileTest.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-if(file_get_contents('status.ini')=='1'){
-    file_put_contents('status.ini','0');
-}else{
-    file_put_contents('status.ini','1');
-}
-print(file_get_contents('status.ini'));
