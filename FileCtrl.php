b5cba0133cc45f32e1d16e89a2483abaac769d4f
WolfHugo <1031629044@qq.com>
2019-11-05 10:08:41 +0800

    FileCtrl
    for now
    RouterCtrl
    DataTime
    FileCtrl

--- FileCtrl.php
+++ FileCtrl.php
@@ -1,10 +1,26 @@
 <?php
 
 class FileCtrl{
-    private $filePath;
-    private $dataArray=array();
-    private $dataTag='temp';
-
-    public function 
-
+    private $filePath=NULL;
+    public function setFilePath($filePath){
+        $this->filePath=$filePath;
+    }
+    public function getFilePath(){
+        return $this->filePath;
+    }
+    public function __construct($filePath)
+    {
+        $this->setFilePath($filePath);
+    }
+    public function getFileCon(){
+        if(file_exists($this->getFilePath())){
+            return file_get_contents($this->getFilePath());
+        }else{
+            return '-1';
+        }
+    }
 }
+
+//usage
+// $fileCtrl=new FileCtrl('status.ini');
+// print($fileCtrl->getFileCon());
