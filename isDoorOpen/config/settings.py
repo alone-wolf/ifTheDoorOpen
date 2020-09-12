980c4393169cc308b84653c2a8d9ede8b07dc265
alone-wolf <1031629044@qq.com>
2020-09-12 20:37:29 +0800

    Update settings.py

--- isDoorOpen/config/settings.py
+++ isDoorOpen/config/settings.py
@@ -5,8 +5,4 @@ class settings:
     SQLALCHEMY_DATABASE_URI = "sqlite:///../data/data.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    MARKDOWN_FOLDER = "data/markdown/"
-
-    VISITCOUNTER_TABLENAME = "visit_counter"
-
     DoorIsOpen = False
