949ff16453cde64bef0ba6b2cf2a48fb4298a5d7
alone-wolf <1031629044@qq.com>
2021-03-10 15:45:40 +0800

    update

--- isDoorOpen/apps/R/__init__.py
+++
@@ -1,6 +0,0 @@
-from isDoorOpen.apps.R.routes import R_routes
-
-
-def init_app_r(server):
-    server.register_blueprint(R_routes)
-    return server
--- isDoorOpen/apps/__init__.py
+++ isDoorOpen/apps/__init__.py
@@ -1,8 +1,6 @@
 from isDoorOpen.apps.Door import init_app_door
-from isDoorOpen.apps.R import init_app_r
 
 
 def init_app(server):
     init_app_door(server)
-    init_app_r(server)
     return server
